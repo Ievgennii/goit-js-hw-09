0764c20a91e932c650fe0b83ef8f5a7d25025f22
Ievgennii <113474106+Ievgennii@users.noreply.github.com>
2023-03-07 20:35:04 +0000

    Deploying to gh-pages from @ Ievgennii/goit-js-hw-09@f98534760dadd20ee416263a6c7055ee03e14efc 🚀

---
+++ 01-color-switcher.e85a7bf0.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");function a(){var a="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));document.body.style.backgroundColor=a,t.disabled=!0,e.disabled=!1}t.addEventListener("click",(function(){timerId=setInterval(a,1e3)})),e.addEventListener("click",(function(){clearInterval(timerId),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.e85a7bf0.js.map
