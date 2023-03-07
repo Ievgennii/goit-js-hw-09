f98534760dadd20ee416263a6c7055ee03e14efc
Ievgennii <simonovievgenii@gmail.com>
2023-03-07 22:34:43 +0200

    color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
+
+btnStart.addEventListener('click', () => {
+  timerId = setInterval(onChangeColor, 1000);
+});
+
+btnStop.addEventListener('click', stopChangeColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function onChangeColor() {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+}
+
+function stopChangeColor() {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+}
