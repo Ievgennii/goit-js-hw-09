d077d709e1417949eff36c14d4a934afe9da473c
Ievgennii <simonovievgenii@gmail.com>
2023-03-07 22:53:14 +0200

    define timerId

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,7 @@
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
 
+let timerId;
 
 btnStart.addEventListener('click', () => {
   timerId = setInterval(onChangeColor, 1000);
