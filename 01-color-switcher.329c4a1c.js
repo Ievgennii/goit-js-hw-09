f50ac892038b5d0c99c3154c4ca403d570620d5d
Ievgennii <113474106+Ievgennii@users.noreply.github.com>
2023-03-07 21:13:59 +0000

    Deploying to gh-pages from @ Ievgennii/goit-js-hw-09@aacca945028738b3ee032d4afa1dc2e8ca62ee97 🚀

--- 01-color-switcher.329c4a1c.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let d;function a(){const d=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=d,t.disabled=!0,e.disabled=!1}t.addEventListener("click",(()=>{d=setInterval(a,1e3)})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.329c4a1c.js.map
---
+++ 01-color-switcher.d598a2f2.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let o;function d(){const o=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=o,console.log(o),t.disabled=!0,e.disabled=!1}t.addEventListener("click",(()=>{o=setInterval(d,1e3)})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1,e.disabled=!0,document.body.style.backgroundColor="#fff"}));
+//# sourceMappingURL=01-color-switcher.d598a2f2.js.map
