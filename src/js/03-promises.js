ea971ad9e5a83cc3cbda454d143f4bfa87d2ab07
Ievgennii <simonovievgenii@gmail.com>
2023-03-10 19:24:40 +0200

    promis with notiflix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,38 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  const delay = parseInt(form.elements.delay.value);
+  const step = parseInt(form.elements.step.value);
+  const amount = parseInt(form.elements.amount.value);
+
+  for (let position = 1; position <= amount; position++) {
+    createPromise(position, delay + position * step)
+      .then(({ position, delay }) => {
+        console.log(
+          `✅ Fulfilled promise ${position} in ${delay + position * step}ms`
+        );
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay + position * step}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        console.log(
+          `❌ Rejected promise ${position} in ${delay + position * step}ms`
+        );
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay + position * step}ms`
+        );
+      });
+  }
+  form.reset();
+}
+
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
@@ -10,30 +45,3 @@ function createPromise(position, delay) {
     }, delay);
   });
 }
-
-const form = document.querySelector('.form');
-
-// function on
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-
-
-  const delay = parseInt(form.elements.delay.value);
-  const step = parseInt(form.elements.step.value);
-  const amount = parseInt(form.elements.amount.value);
-
-  for (let i = 1; i <= amount; i++) {
-    const position = i + 1;
-    const promiseDelay = delay + i * step;
-    createPromise(i, delay + i * step)
-      .then(({ position, delay }) => {
-        console.log(`✅ Fulfilled promise ${i} in ${delay + i * step}ms`);
-      })
-      .catch(({ position, delay }) => {
-        console.log(`❌ Rejected promise ${i} in ${delay + i * step}ms`);
-      });
-  }
-  form.reset();
-});
